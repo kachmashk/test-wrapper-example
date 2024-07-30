3b3cf49e5eace0dda3756bdbcdbbf752cb9be51d
Oskar Kaczmarzyk <oskar@kaczmarzyk.dev>
2024-07-30 17:27:29 +0200

    added tamagui, added @tamagui/config

--- app/Home.tsx
+++ app/Home.tsx
@@ -1,12 +1,15 @@
+import { Button } from '@/components'
 import { Stack } from 'expo-router'
 import { FC } from 'react'
-import { Text } from 'react-native'
+import { YStack } from 'tamagui'
 
 export const Home: FC = () => {
   return (
     <>
-      <Stack.Screen options={{ title: 'Home' }} />
-      <Text>Text</Text>
+      <Stack.Screen options={{ title: 'test wrapper example' }} />
+      <YStack flex={1} justifyContent='center' alignItems='center'>
+        <Button label='Label' onPress={() => console.log('test')} />
+      </YStack>
     </>
   )
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,33 @@
+import { tamaguiConfig } from '@/tamagui-config'
+import { useFonts } from 'expo-font'
 import { Stack } from 'expo-router'
+import * as SplashScreen from 'expo-splash-screen'
+import { useEffect } from 'react'
+import 'react-native-reanimated'
+
+import { TamaguiProvider } from 'tamagui'
+
+SplashScreen.preventAutoHideAsync()
 
 export default function HomeLayout() {
-  return <Stack />
+  const [loaded] = useFonts({
+    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+  })
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [loaded])
+
+  if (!loaded) {
+    return null
+  }
+
+  return (
+    <TamaguiProvider config={tamaguiConfig}>
+      <Stack />
+    </TamaguiProvider>
+  )
 }
