a434deafec76dbd7b16aa3ad796868d92fbf9697
Oskar Kaczmarzyk <oskar@kaczmarzyk.dev>
2024-08-06 14:43:08 +0200

    first solution

--- components/Button/Button.test.tsx
+++ components/Button/Button.test.tsx
@@ -2,30 +2,31 @@ import { tamaguiConfig } from '@/tamagui-config'
 import { Button, ButtonProps } from './Button'
 import { render, screen, fireEvent } from '@testing-library/react-native'
 import { TamaguiProvider } from 'tamagui'
+import { FC } from 'react'
 
 const defaultProps: ButtonProps = {
   label: 'Label',
   onPress: jest.fn(),
 }
 
+const Component: FC<ButtonProps> = (props) => {
+  return (
+    <TamaguiProvider config={tamaguiConfig}>
+      <Button {...props} />
+    </TamaguiProvider>
+  )
+}
+
 describe('Button', () => {
   it('renders label', () => {
     const label = 'My custom label'
-    render(
-      <TamaguiProvider config={tamaguiConfig}>
-        <Button {...defaultProps} label={label} />
-      </TamaguiProvider>
-    )
+    render(<Component {...defaultProps} label={label} />)
     expect(screen.getByText(label)).toBeOnTheScreen()
   })
 
   it('calls onPress', () => {
     const onPress = jest.fn()
-    render(
-      <TamaguiProvider config={tamaguiConfig}>
-        <Button {...defaultProps} onPress={onPress} />
-      </TamaguiProvider>
-    )
+    render(<Component {...defaultProps} onPress={onPress} />)
 
     fireEvent.press(screen.getByTestId('ButtonID'))
     expect(onPress).toHaveBeenCalled()
